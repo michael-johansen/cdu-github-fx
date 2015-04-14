0d6a8ecbd038e6452803c12cef9522a2f72fc1a0
Michael Johansen <michaeljohansen83@gmail.com>
2015-04-14 17:43:56 +0200

    Moving layout out of code

--- app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
+++ app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
@@ -1,16 +1,15 @@
 package cdu.fx.hello;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 
 public class HelloWorldApplication extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /** This is ugly! Move this to fxml */
-        primaryStage.setScene(new Scene(new Label("Hello CDU!")));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        primaryStage.setScene(fxmlLoader.load());
         primaryStage.show();
     }
 }
