2af4b8905311f27c69337b07366d84e1df316f38
Michael Johansen <michaeljohansen83@gmail.com>
2015-04-14 17:33:53 +0200

    Added comment about how ugly the code is

--- app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
+++ app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 public class HelloWorldApplication extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
+        /** This is ugly! Move this to fxml */
         primaryStage.setScene(new Scene(new Label("Hello CDU!")));
         primaryStage.show();
     }
