0ed4d54d2391b4519b6697fe32de864a9641b8f6
Michael Johansen <michaeljohansen83@gmail.com>
2015-04-15 10:14:49 +0200

    Added localization of message

--- app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
+++ app-fx/src/main/java/cdu/fx/hello/HelloWorldApplication.java
@@ -4,11 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class HelloWorldApplication extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Locale locale = new Locale(getParameters().getNamed().get("locale"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("messages", locale));
         primaryStage.setScene(fxmlLoader.load());
         primaryStage.show();
     }
