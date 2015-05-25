192bf0440d7ec73228a0d9320a61436443403a13
Michael Johansen <michaeljohansen83@gmail.com>
2015-05-25 16:06:09 +0200

    Commented about required changes to add databinding.

--- app-fx/src/main/java/cdu/fx/hello/HelloWorldController.java
+++ app-fx/src/main/java/cdu/fx/hello/HelloWorldController.java
@@ -2,9 +2,13 @@ package cdu.fx.hello;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.fxml.Initializable;
 
 /**
  * Created by Michael on 25.05.2015.
+ * TODO: Must implement Initializable, so that we can set up data binding. Bind input field text to custom property.
+ * TODO: Let the controller know about the fields in the fxml file (@FXML annotation)
+ *
  */
 public class HelloWorldController {
     public void changeToNorwegian(ActionEvent actionEvent) {
