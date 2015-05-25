38363f3db8a27bf4f5a8876535e720d18d69d9f9
Michael Johansen <michaeljohansen83@gmail.com>
2015-05-25 16:24:27 +0200

    Added some custom databindings

--- app-fx/src/main/java/cdu/fx/hello/HelloWorldController.java
+++ app-fx/src/main/java/cdu/fx/hello/HelloWorldController.java
@@ -1,16 +1,40 @@
 package cdu.fx.hello;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringExpression;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Michael on 25.05.2015.
- * TODO: Must implement Initializable, so that we can set up data binding. Bind input field text to custom property.
- * TODO: Let the controller know about the fields in the fxml file (@FXML annotation)
- *
  */
-public class HelloWorldController {
+public class HelloWorldController implements Initializable {
+    @FXML
+    public TextField myInput;
+    @FXML
+    public Label myLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        StringExpression greeting = Bindings.concat(
+                resources.getString("greeting"),
+                " ",
+                myInput.textProperty(),
+                "(",
+                myInput.textProperty().length(),
+                ")!"
+        );
+        myLabel.textProperty().bind(greeting);
+        myLabel.visibleProperty().bind(myInput.textProperty().isNotEmpty());
+    }
+
     public void changeToNorwegian(ActionEvent actionEvent) {
         Event.fireEvent(actionEvent.getTarget(), new ChangeLocaleEvent("no"));
     }
