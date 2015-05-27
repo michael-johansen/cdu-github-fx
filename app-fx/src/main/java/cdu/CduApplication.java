a3c3f3e85f10ac0b529806f6866f67ce53faa4f6
Michael Johansen <michaeljohansen83@gmail.com>
2015-05-28 01:07:26 +0200

    Step 1

---
+++ app-fx/src/main/java/cdu/CduApplication.java
@@ -0,0 +1,14 @@
+package cdu;
+
+/**
+ * Created by Michael on 28.05.2015.
+ */
+public class CduApplication {
+    // Extend application
+    // Add Scene
+    // Add Label
+
+    public static void main(String[] args) {
+        // launch(args)
+    }
+}
