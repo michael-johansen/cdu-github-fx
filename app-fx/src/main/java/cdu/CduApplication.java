109f0bfdc57fc72bca43367a93bd01d29e79985f
Michael Johansen <michaeljohansen83@gmail.com>
2015-05-28 01:14:28 +0200

    Added hello world

--- app-fx/src/main/java/cdu/CduApplication.java
+++ app-fx/src/main/java/cdu/CduApplication.java
@@ -1,14 +1,22 @@
 package cdu;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Created by Michael on 28.05.2015.
  */
-public class CduApplication {
-    // Extend application
-    // Add Scene
-    // Add Label
+public class CduApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new Label("Hello CDU!!")));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        // launch(args)
+        launch(args);
     }
 }
